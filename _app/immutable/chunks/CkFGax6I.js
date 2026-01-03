63accc385241d2d9ef5848cb065338683839c7b7
alfonso.briones <alexanderbvart@gmail.com>
2026-01-03 06:00:15 -0500

    Updates

---
+++ _app/immutable/chunks/CkFGax6I.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_fz4bpu?.base??"/chakra",a=globalThis.__sveltekit_fz4bpu?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/_ugkv9Jx.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_54yo7w?.base??"/chakra",a=globalThis.__sveltekit_54yo7w?.assets??s??"";export{a,s as b};
